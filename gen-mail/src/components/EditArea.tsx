6c86e5b77d9fc252531b326fff4051f4b79bc6ec
satokiohashi <nifty.co.jp>
2023-01-03 17:08:27 -0800

    Save Changes

--- gen-mail/src/components/EditArea.tsx
+++ gen-mail/src/components/EditArea.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import { Box, Flex, Spacer, Text, Textarea, Button, useClipboard } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { setEmitFlags } from 'typescript';
+import GetEditerLogo from '../data/GetEditerLogo';
 
 const countWords = (str : string) => {
   const arr = str.split(' ');
   return arr.filter(word => word !== '').length;
 }
+
+const Logos: string[] = ['BoldText', 'ItalicText', 'UnderlineText']
 
 const EditArea = () => {
   const { onCopy, value, setValue, hasCopied } = useClipboard("");
@@ -13,6 +17,15 @@ const EditArea = () => {
   return (
     <Box position={'sticky'} >
       <Text fontWeight={'bold'} width={'100%'}>Paste your email</Text>
+      <StyledLogos>
+        {Logos.map((logo, index) => {
+          return (
+            <li key={index}>
+              {GetEditerLogo(logo)}
+            </li>
+          )
+        })}
+      </StyledLogos>
       <Textarea 
       bg='white'
         height={'90vh'} 
@@ -27,9 +40,15 @@ const EditArea = () => {
         <Spacer />
         <Text mr={2} >Words: {countWords(value)}</Text>
       </Flex>
-      
     </Box>
   )
 }
+
+const StyledLogos = styled('ul')`
+  display: flex;
+  list-style-type: none;
+  margin: 10px 5px;
+
+`
 
 export default EditArea;
