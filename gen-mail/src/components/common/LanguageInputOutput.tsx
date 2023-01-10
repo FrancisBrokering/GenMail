c57be59557da19eda67d09942535d2bacd1f6b48
Francis Brokering <francisbrokering@gmail.com>
2023-01-10 11:26:54 -0800

    added language selection menu dropdown

--- gen-mail/src/components/common/LanguageInputOutput.tsx
+++ gen-mail/src/components/common/LanguageInputOutput.tsx
@@ -7,18 +7,28 @@ import {
   Flex,
   Select,
   Center,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
-import { ArrowRightIcon } from "@chakra-ui/icons";
-import React, { useEffect } from "react";
+import { ArrowRightIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import React, { useEffect, useState } from "react";
+import { ReactComponent as JapanFlag } from "../../assets/icons/Japan.svg";
+import { ReactComponent as UsaFlag } from "../../assets/icons/USA.svg";
+//Use this link for flag SVGs https://uxwing.com/usa-flag-round-circle-icon/
 
 type LanguageInputOutputProps = {
   pageTitle: string;
   setLanguage: (language: string) => void;
 };
 
+//TODO: when adding more languages, need to change props to have setOutputLanguage() and setInputLanguage(). setLanguage is currently serving no purpose 
 const LanguageInputOutput = (props: LanguageInputOutputProps) => {
   const { t, i18n } = useTranslation();
+  const [userInputLanguageLocalVer, setUserInputLanguageLocalVer] = useState("ja");
+  const [userOutputLanguageLocalVer, setUserOutputLanguageLocalVer] = useState("en");
   return (
     <Box>
       <Text textAlign="center" mb="50px" fontWeight="bold" fontSize="20px">
@@ -26,16 +36,45 @@ const LanguageInputOutput = (props: LanguageInputOutputProps) => {
       </Text>
       <FormLabel>①{t("selectLang")}</FormLabel>
       <Flex>
-        <Select onChange={(e) => props.setLanguage(e.target.value)} w="300px">
-          <option value="ja">{t("japanese")} 🇯🇵</option>
-          <option value="en">{t("english")} 🇺🇸</option>
-        </Select>
+
+      <Menu>
+          <MenuButton as={Button}
+            leftIcon={userInputLanguageLocalVer === "ja" ? <JapanFlag margin-right='12px' width='22px' height='22px' /> : <UsaFlag margin-right='12px' width='22px' height='22px' />}
+            rightIcon={<ChevronDownIcon />}
+            variant='outline'
+            borderColor='gray.300'
+          >
+            <Text fontWeight='500'>{userInputLanguageLocalVer === "ja" ? t("japanese") : t("english")}</Text>
+          </MenuButton>
+          <MenuList>
+            <MenuItem minH='48px' onClick={(e) => {props.setLanguage("ja");setUserInputLanguageLocalVer("ja")}} icon={<JapanFlag margin-right='12px' width='22px' height='22px' />}>
+              <Text >{t("japanese")}</Text>
+            </MenuItem>
+            <MenuItem minH='40px' onClick={(e) => {props.setLanguage("en");setUserInputLanguageLocalVer("en")}} icon={<UsaFlag margin-right='12px' width='22px' height='22px' />}>
+              <Text >{t("english")}</Text>
+            </MenuItem>
+          </MenuList>
+        </Menu>
+
         <Center pl="20px" pr="20px">
           <ArrowRightIcon w={6} h={6} color="cyan.400" />
         </Center>
-        <Select w="300px">
-          <option value="en">{t("english")} 🇺🇸</option>
-        </Select>
+
+        <Menu>
+          <MenuButton as={Button}
+            leftIcon={userOutputLanguageLocalVer === "ja" ? <JapanFlag margin-right='12px' width='22px' height='22px' /> : <UsaFlag margin-right='12px' width='22px' height='22px' />}
+            rightIcon={<ChevronDownIcon />}
+            variant='outline'
+            borderColor='gray.300'
+          >
+            <Text fontWeight='500'>{userOutputLanguageLocalVer === "ja" ? t("japanese") : t("english")}</Text>
+          </MenuButton>
+          <MenuList>
+            <MenuItem minH='40px' onClick={(e) => {props.setLanguage("en");setUserOutputLanguageLocalVer("en")}} icon={<UsaFlag margin-right='12px' width='22px' height='22px' />}>
+              <Text >{t("english")}</Text>
+            </MenuItem>
+          </MenuList>
+        </Menu>
       </Flex>
     </Box>
   );
