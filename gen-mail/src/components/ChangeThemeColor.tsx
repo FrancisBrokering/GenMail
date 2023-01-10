560f500524b3adaa3d4aecd82c1fc44f29b8671f
satokiohashi <nifty.co.jp>
2023-01-10 12:44:52 -0800

    changeTheme Button added, deleted unused Logos

--- gen-mail/src/components/ChangeThemeColor.tsx
+++ gen-mail/src/components/ChangeThemeColor.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { useColorMode, FormControl, FormLabel, Switch, Flex } from '@chakra-ui/react';
+import { useColorMode, FormControl, FormLabel, Switch, Flex, Button, Box, Text, useColorModeValue } from '@chakra-ui/react';
 import { useTranslation } from "react-i18next";
+import { ReactComponent as DarkMoon } from "../assets/icons/DarkMoon.svg";
+import { ReactComponent as LightSun } from "../assets/icons/LightSun.svg";
 
 const ChangeThemeColor = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+  const ThemeButton_Bg = useColorModeValue("gray.300", "gray.700")
   const { t } = useTranslation();
 
   console.log(colorMode)
   return (
-    <FormControl ml={"25px"} mt="20px">
-      <Flex>
+    <Box ml={"25px"} mt="20px">
+      <Button
+        bg={"transparent"}
+        _hover={{ bg: ThemeButton_Bg}}
+        onClick={toggleColorMode}
+      >
+        <Flex alignItems={"center"}>
+          {colorMode === "dark" ? <LightSun /> : <DarkMoon />}
+          <Text ml={"10px"}>{colorMode === "dark" ? t("lightMode") : t("darkMode")}</Text>
+        </Flex>
+      </Button>
+      {/* <Flex>
         <FormLabel>{colorMode === "dark" ? t("lightMode") : t("darkMode")}</FormLabel>
         <Switch
           size={"md"}
@@ -17,8 +30,8 @@ const ChangeThemeColor = () => {
           defaultChecked={colorMode === "dark" ? true : false}
           onChange={toggleColorMode}
         />
-      </Flex>
-    </FormControl>
+      </Flex> */}
+    </Box>
   )
 }
 
